ee059ecbf71bcfcf72a89d1c7378aaca089bc715
LyazginD <lyazg10@mail.ru>
2024-11-26 16:25:18 +0500

    added levels to logger

--- internal/env/env.go
+++ internal/env/env.go
@@ -4,10 +4,12 @@ import utilenv "javacode-test/util/env"
 
 var (
 	POSTGRES_CONN string
+	LOG_LEVEL     string
 )
 
 func init() {
 	utilenv.LoadFileEnv("./config/config.env")
 
 	utilenv.LoadStrVar(&POSTGRES_CONN, "POSTGRES_CONN")
+	utilenv.LoadStrVar(&LOG_LEVEL, "LOG_LEVEL")
 }
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,13 +1,26 @@
 package logger
 
 import (
+	"javacode-test/internal/env"
 	"log/slog"
 	"os"
 )
 
+const (
+	envLocal = "local"
+	envProd  = "prod"
+)
+
 func SetupLogger() *slog.Logger {
 	var log *slog.Logger
-	log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	switch env.LOG_LEVEL {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case envProd:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
 
 	return log
 }
